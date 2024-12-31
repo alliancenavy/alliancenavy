5f1d10c708fb79fcf0d01cff0d5efa4d692cb44e
JsonBacker <jasonbacker@gmail.com>
2024-12-31 19:26:27 +0000

    Added Favicons

--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,5 @@
 window.onload = () => {
     const h1 = document.querySelector('h1');
     h1.style.color = 'red';
-    h1.textContent = 'This is a test - js works!';
+    h1.textContent = 'This is placeholder text';
 };
